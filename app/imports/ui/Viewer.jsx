dc7c8e738f21560c3d9a8a97923943c16b603588
Moritz Vifian <moritz.vifian@gmail.com>
2019-08-09 22:24:56 +0200

    Don't display transposer if no chords are present. Show text in multicolumns Attach new Less file to Jsx file (via import)

--- app/imports/ui/Viewer.jsx
+++ app/imports/ui/Viewer.jsx
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import TranposeSetter from "./TransposeSetter.jsx";
 import ChrodLib from "../api/libchrod.js";
-import { RmdHelpers } from "../api/collections.js";
 import Collapsed from './Collapsed.jsx';
 import ReactDOM from 'react-dom';
 
+import './Viewer.less'
 var Parser = require("html-react-parser");
 
 class Viewer extends Component {
+
+  key = undefined 
+
   constructor() {
     super();
     this.state = { relTranspose: 0 };
@@ -35,75 +38,37 @@ class Viewer extends Component {
   };
 
   render() {
-    let chords = this.props.song.getChords();
-    let chrodlib = new ChrodLib();
-    let rmd_html = this.props.song.getHtml();
-
-    let key = ChrodLib.guessKey(chords);
-
-    // TODO: if key undef, write something there
-
-    let dT = this.state.relTranspose;
-
     // Parse HTML to react-vdom and replace chord values.
-    let vdom = Parser(rmd_html, {
-      replace: function(domNode) {
-        if (domNode.name && domNode.name == 'i' && 'data-chord' in domNode.attribs) {
-          let chord = domNode.attribs['data-chord'];
-          let t = chrodlib.transpose(chord, key, dT);
-          let chord_;
-          if (t == null) {
-            chord_ = <span className="before">{chord}</span>;
-          } else {
-            chord_ = <span className={"before " + t.className}>{t.base}<sup>{t.suff}</sup></span>;
-          }
-          return <i>{chord_}{domNode.children[0].data}</i>;
-        }
-      }
-    });
+    let vdom = this.createVdom();
 
-    // Idee: obige replace-funktion könnte vom TransposeSetter geholt werden. Dadurch könnte der relTranspose-Zustand völlig in 
-    // den TransposeSetter wandern. 
-
-    /*
-    if (this.state.relTranspose != 0) {
-      chordtable = (
-        <table className="chordtable">
-          <tbody>
-            <tr>
-              <td>Orig:</td>
-              {chords.map((c, i) => <td key={i}>{c}</td>)}
-            </tr>
-            <tr>
-              <td>Tran:</td>
-              {chrodlib
-                .transpose(chords, this.state.relTranspose)
-                .map((c, i) => <td key={i}>{c}</td>)}
-            </tr>
-          </tbody>
-        </table>
-      );
-    } else {
-      chordtable = "";
+    let options = []
+    for (let i = 0; i < 6; i++) {
+      options.push(<option value={i}>{i}</option>)
     }
-    */
-
 
     return (
       <div className="container">
+        <div id="inlineSettings">
+        <select id="overrideNumColumns" onChange={this.handleColDropdown}>
+          <option value="Auto">Auto</option>
+          {options}
+        </select>
+          {this.containsLyrics(vdom) && 
+          <section>
+            <TranposeSetter
+              transposeSetter={this.transposeSetter}
+              transpose={this.state.relTranspose}
+              keym={this.key}
+            />
+          </section>
+          }
+        </div>
         <div
           className="content chordsheet"
           id="chordsheet"
           onContextMenu={this.handleContextMenu}
         >
-          <section>
-            <TranposeSetter
-              transposeSetter={this.transposeSetter}
-              transpose={this.state.relTranspose}
-              keym={key}
-            />
-          </section>
-          <section ref="html">
+          <section ref="html" id="chordSheetContent">
             {vdom}
           </section>
         </div>
@@ -113,6 +78,45 @@ class Viewer extends Component {
         </Collapsed>
       </div>
     );
+  }
+
+  handleColDropdown( ev ) {
+    console.log(ev)
+  }
+  
+  containsLyrics() {
+    let chords = this.props.song.getChords()
+    return chords && chords.length>0
+  }
+
+  createVdom() {
+    let chrodlib = new ChrodLib();
+    let chords = this.props.song.getChords();
+    let rmd_html = this.props.song.getHtml();
+
+    this.key = ChrodLib.guessKey(chords);
+    let key = this.key;
+
+
+    // TODO: if key undef, write something there
+    let dT = this.state.relTranspose;
+
+    return Parser(rmd_html, {
+      replace: function (domNode) {
+        if (domNode.name && domNode.name == 'i' && 'data-chord' in domNode.attribs) {
+          let chord = domNode.attribs['data-chord'];
+          let t = chrodlib.transpose(chord, key, dT);
+          let chord_;
+          if (t == null) {
+            chord_ = <span className="before">{chord}</span>;
+          }
+          else {
+            chord_ = <span className={"before " + t.className}>{t.base}<sup>{t.suff}</sup></span>;
+          }
+          return <i>{chord_}{domNode.children[0].data}</i>;
+        }
+      }
+    });
   }
 }
 

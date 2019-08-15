beebf57e9804cd56d1027069c9cd1fe4772e792c
Moritz Vifian <moritz.vifian@gmail.com>
2019-08-15 15:30:54 +0200

    Improving slider (selected key is now shown inside the circle)

--- app/imports/ui/TransposeSetter.jsx
+++ app/imports/ui/TransposeSetter.jsx
@@ -2,47 +2,23 @@ import React, { Component } from "react";
 import ChrodLib from "../api/libchrod.js";
 import PropTypes from "prop-types";
 import Slider from "rc-slider";
+import Tooltip from 'rc-tooltip';
 import "rc-slider/assets/index.css";
-import "rc-tooltip/assets/bootstrap.css";
+import "rc-tooltip/assets/bootstrap.css"
+import './transpose-setter.less'
 
-var intervals = new Map([
-  [0, "Original"],
-  [1, "Kl. Sekunde"],
-  [2, "Gr. Sekunde"],
-  [3, "Kl. Terz"],
-  [4, "Gr. Terz"],
-  [5, "Quart"],
-  [6, "Tritonus"],
-  [7, "Quint"],
-  [8, "Kl. Sext"],
-  [9, "Gr. Sext"],
-  [10, "Kl. Sept"],
-  [11, "Gr. Sept"],
-  [12, "Oktave "],
-  [13, "Kl. None"],
-  [14, "Gr. None"],
-  [15, "Kl. Dezime"],
-  [16, "Gr. Dezime"],
-  [17, "Kl. Undezime"]
-]);
+const Handle = Slider.Handle
+
 
 export default class TranposeSetter extends Component {
   constructor(props) {
     super(props);
+    this.keyObjs = {}
   }
 
   handleSlider = value => {
     this.props.transposeSetter(Number.parseInt(value));
   };
-
-  static equivalentShift(pitch) {
-    let pit = Number.parseInt(pitch);
-    let int = pit / 12;
-    let add = Math.ceil(Math.abs(int)) * 12;
-    let op = add - Math.abs(pit); // because of the idiotic (unnecessary intervals >12)
-    let str = this.intFromPitch(op * Math.sign(pit) * -1);
-    return str;
-  }
 
   tipFormatter = v => {
     return v;
@@ -50,37 +26,41 @@ export default class TranposeSetter extends Component {
 
   // Inherited from React.Component
   render() {
-    // TODO: make object and calculate resulting key
-    let marks = {
-      "-7": -7,
-      '-3': -3,
-      // TODO: key here
-      0: 'Original',
-      3: "+3",
-      7: "+7"
-    };
+    let marks = {};
     if ( this.props.keym) {
       let key = this.props.keym;
       let keys = {};
+      this.keyObjs = {};
       let libChrod = new ChrodLib();
-      for (var i=-7; i<=7; i++) {
+      for (let i=-7; i<=7; i++) {
         let keyobj = libChrod.shift(key, i);
-        keys[i] = keyobj.key;  
-        if (i==0) { keys[i]+=" "+key.scale}
+        this.keyObjs[i] = keyobj;
+        const pm = i > 0 ? '+' : ' '
+        keys[i] =<><i>{keyobj.key}</i>{pm + i}</> ;  
+        if (i==0) { keys[i] = <><i>{keyobj.key}</i> {keyobj.scale}</> }
       }
       marks = keys;
+    } else {
+      for (let i = -7; i <= 7; i++) {
+        const pm = i>0 ? '+': ' '
+        this.keyOjbs[i] = {key: i, scale: '?'};
+        marks[i] = pm + i + ' ht'
+      }
     }
     return (
+      // TODO: responsive-> for size s change to 
       <div id="transposer">
           <Slider
             id="typeinp"
             min={-7}
             max={7}
             name="relTranspose"
+            handle = {this.handle}
             value={this.props.transpose}
             onChange={this.handleSlider}
             marks = {marks}
             step={1}
+            included = {false}
             tipFormatter = {this.tipFormatter}
             dots
             vertical={true}
@@ -88,14 +68,21 @@ export default class TranposeSetter extends Component {
       </div>
     );
   }
-  /**
-   * 
-   * @param {number} i 
-   */
-  static intFromPitch(i) {
-    let vz = i < 0 ? "-" : "+";
-    return vz + Math.abs(i) + " Halbtöne = " + vz + intervals.get(Math.abs(i));
-  }
+
+  handle = (props) => {
+    // <Handle value={value} {...restProps} />
+    const { className, tipTransitionName, tipFormatter, vertical, offset, value } = props;
+    const { dragging, noTip } = props;
+
+    const style = vertical ? { bottom: offset + '%' } : { left: offset + '%' };
+    const handle = (<div className={className} style={style}>
+      {this.keyObjs[value].key}
+    </div>);
+
+    return handle;
+  };
+
+
 }
 
 TranposeSetter.propTypes = {

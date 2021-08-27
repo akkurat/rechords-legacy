569938827b31e0db763951f25407a7e6bdc02833
Moritz Vifian <moritz.vifian@gmx.ch>
2021-08-27 11:03:19 +0200

    QuickInput working
    TODO:
    Show buttons nicely
    
    Buttons for QuickInput

--- app/imports/ui/PdfViewer/QuickInput.tsx
+++ app/imports/ui/PdfViewer/QuickInput.tsx
@@ -2,11 +2,10 @@ import * as React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { FormEvent, FunctionComponent } from 'react'
-// import './quick_input.less'
 
 export type keysEvent = React.MouseEvent<HTMLButtonElement, MouseEvent> | React.KeyboardEvent<HTMLInputElement> | KeyboardEvent
 type dec = [(e: keysEvent)=> boolean, string, string, number]
-const updowns: dec[] =[ 
+const updowns: dec[] =[
   [mult1,'*','/',1.2],
   [mult2, '*','/',2],
   [inc0, '+','-',0.1],
@@ -18,14 +17,16 @@ const updowns: dec[] =[
 export const QuickInput: FunctionComponent<{id: string, value: number, onChange: (a: number) => void}> = ({onChange, value,id}) => {
 
 
+  const [mod, setMod] = useState(updowns[3])
 
-  const [mod, setMod] = useState({up:'+', down:'-', inc: 1})
-
-  useEffect(() => 
+  useEffect(() => {
     document.addEventListener('keydown', e => {
       setMod(decider(e))
-
-    } ))
+    })
+    document.addEventListener('keyup', e => {
+      setMod(decider(e))
+    })
+  })
 
   const up = (e: keysEvent) => {
     e.preventDefault()
@@ -48,11 +49,15 @@ export const QuickInput: FunctionComponent<{id: string, value: number, onChange:
     }
   }
 
-  return <><input type="number" step="1" min="1" max="200" 
-    onKeyDown={handleKey} onInput={handleInput} id={id}
-    value={value} onChange={ev => onChange( parseFloat(ev.currentTarget.value))} /> 
-  <button onClick={ev => up(ev)}></button>
-  <button onClick={ev => down(ev)}></button>
+  const [,u,d,inc] = mod
+  return <>
+    <button onClick={ev => down(ev)}>{d}</button>
+    <input type="number" step="1" min="1" max="200"
+    onKeyDown={handleKey} id={id}
+    value={value} onChange={ev => onChange( parseFloat(ev.currentTarget.value))} />
+  <button onClick={ev => up(ev)}>{u}</button>
+    {inc}
+
   </>
 }
 
@@ -68,7 +73,7 @@ function mult1(keyEvent: keysEvent) {
   return keyEvent.altKey
 }
 
-function decider(e: keysEvent) {
+function decider(e: keysEvent): dec {
   for( const ud of updowns ) {
     if( ud[0](e) ) return ud
   }

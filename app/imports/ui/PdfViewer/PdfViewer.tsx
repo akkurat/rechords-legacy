b0a0ef042e7245f760fbdc948d1be7dde020024a
Moritz Vifian <moritz.vifian@gmx.ch>
2021-08-30 03:06:36 +0200

    Animation when changing settings

--- app/imports/ui/PdfViewer/PdfViewer.tsx
+++ app/imports/ui/PdfViewer/PdfViewer.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react'
 
 import { IViewerProps } from '../Viewer'
-import { PdfBlob } from './PdfBlob'
+import { PdfObject } from './PdfObject'
 import { IPdfViewerSettings, PdfSettings, PdfViewerStates } from './PdfSettings'
 import { jsPdfGenerator } from './PdfRenderer'
 import { debounce } from 'underscore'
 import Drawer from '../Drawer'
 import { NavLink } from 'react-router-dom'
 import './PdfViewer.less'
-import { search } from 'core-js/fn/symbol'
+import classNames from 'classnames'
+import { clef } from './SettingIcons'
+import { PDF } from '../Icons.jsx'
+import { useState } from 'react'
 
 const debug = true
 
-export class PdfViewer extends React.Component<IViewerProps, { pdfBlobUrl: string }> {
+export class PdfViewer extends React.Component<IViewerProps, { pdfBlobUrl: string, loading: boolean }> {
   first: boolean
   constructor(props: IViewerProps) {
     super(props)
-    this.state = { pdfBlobUrl: '' }
+    this.state = { pdfBlobUrl: '', loading: true }
   }
 
 
@@ -30,16 +33,17 @@ export class PdfViewer extends React.Component<IViewerProps, { pdfBlobUrl: strin
       this.setState(
         os => {
           setTimeout(() => URL.revokeObjectURL(os.pdfBlobUrl), 10e3) // freeing old url for memory
+          setTimeout(() => this.setState({loading: false}), 2e3 )
           return { pdfBlobUrl }
         }
       )
     }
 
-    _setSettings = debounce((a: IPdfViewerSettings) => this.generatePdf(a), 1500)
+    _setSettings = debounce((a: IPdfViewerSettings) => this.generatePdf(a), 500)
 
     setSettings = (settings: IPdfViewerSettings) =>  {
       if( this.first ) { this.first = false; this.generatePdf(settings)}
-      else { this._setSettings(settings) }
+      else { this.setState({loading:true}); this._setSettings(settings) }
     }
 
 
@@ -51,13 +55,16 @@ export class PdfViewer extends React.Component<IViewerProps, { pdfBlobUrl: strin
 
       const s = this.props.song
 
+
       return <>
         <Drawer open={true} id="pdfsettings">
           <NavLink to={`/view/${s.author_}/${s.title_}`} >x</NavLink>
           <PdfSettings consumer={this.setSettings} songId={this.props.song._id} />
         </Drawer>
-        <div className="content">
-          <PdfBlob url={this.state.pdfBlobUrl}></PdfBlob>
+        <div className={classNames({pdfgrid: true, loading: this.state.loading})} >
+          <div className="loading"></div>
+          <PdfSpinner />
+          <PdfObject url={this.state.pdfBlobUrl}></PdfObject>
         </div>
       </>
 
@@ -73,4 +80,18 @@ export class PdfViewer extends React.Component<IViewerProps, { pdfBlobUrl: strin
 
 
 
+const icons = [<PDF />, clef]
+const PdfSpinner: React.FunctionComponent<{}> = () => {
+
+  const [idx, setIdx] = useState(0)
+
+  const icon = icons[idx]
+
+  const handleAnimationEnd = (ev) => {
+    setIdx( (idx+1)%2 )
+  }
+
+  return <div className="spinner" 
+  onAnimationIteration={handleAnimationEnd}> {icon} </div>
+}
 
